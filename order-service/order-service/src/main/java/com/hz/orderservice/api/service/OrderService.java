713b9592ec7c084eb664e681def6cf92eee9ff37
hafeez <dhz939127@gmail.com>
2024-03-17 10:06:07 +0530

    added cloud config

--- order-service/order-service/src/main/java/com/hz/orderservice/api/service/OrderService.java
+++ order-service/order-service/src/main/java/com/hz/orderservice/api/service/OrderService.java
@@ -7,16 +7,25 @@ import com.hz.orderservice.api.common.TransactionResponse;
 import com.hz.orderservice.api.entity.Order;
 import com.hz.orderservice.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
+    @Lazy
     private RestTemplate template;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
+
 
     public TransactionResponse saveOrder(TransactionRequest transactionRequest){
         String response = "";
@@ -27,7 +36,7 @@ public class OrderService {
         payment.setAmount(savedOrder.getPrice());
 
         // rest call
-        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/do-payment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api, order added to cart";
         return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
 
